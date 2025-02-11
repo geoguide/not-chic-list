f9c1654ba55c09b10aefb6b56e345154ce6b9663
geoguide <geoffguidetti@gmail.com>
2025-02-10 16:12:42 -0800

    update footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,30 +8,7 @@ export default function Home() {
 				<h1 className={styles.title}>This is not Chiclist</h1>
 			</main>
 			<footer className={styles.footer}>
-				<a
-					href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<Image aria-hidden src='/file.svg' alt='File icon' width={16} height={16} />
-					Learn
-				</a>
-				<a
-					href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<Image aria-hidden src='/window.svg' alt='Window icon' width={16} height={16} />
-					Examples
-				</a>
-				<a
-					href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<Image aria-hidden src='/globe.svg' alt='Globe icon' width={16} height={16} />
-					Go to nextjs.org →
-				</a>
+				<h2>To be setup with mantine, supabase, and style</h2>
 			</footer>
 		</div>
 	)
